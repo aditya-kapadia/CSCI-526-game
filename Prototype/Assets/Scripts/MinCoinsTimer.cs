2bcb9625bd4fe91eaae886cba2dcaad84586895d
pvandhana <vandhana@usc.edu>
2022-10-17 21:46:06 -0700

    added minimum number of coins image

---
+++ Prototype/Assets/Scripts/MinCoinsTimer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MinCoinsTimer : MonoBehaviour
+{
+    // Use this for initialization
+    public int destroyTime = 5;
+    void Start()
+    {
+        StartCoroutine(WaitThenDie());
+    }
+
+    IEnumerator WaitThenDie() {
+        yield return new WaitForSeconds(destroyTime);
+        Destroy(gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
