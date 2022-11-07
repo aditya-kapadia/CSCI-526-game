d578641c1d6305a4457e4f8278d67aa16c4ea83b
Soumyaprakash Dasmohaptra <dasmohap@usc.edu>
2022-11-06 16:28:19 -0800

    Added min_coins_image

--- Prototype/Assets/Scripts/starsHandler.cs
+++ Prototype/Assets/Scripts/starsHandler.cs
@@ -8,16 +8,17 @@ public class starsHandler : MonoBehaviour
     public GameObject nextButton;
     public ItemCollector collectorObj;
     public Text text;
+    public Image min_coins;
     // Use this for initialization
     void Start()
     {
-
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+       
     }
 
 
@@ -26,16 +27,19 @@ public class starsHandler : MonoBehaviour
     {
 
         avgCoinsCollected = avgCoinsCollected * 100;
+        min_coins.enabled = false;
         if (avgCoinsCollected < 33)
         {
             Debug.Log("I have zero stars");
-            text.text = "Not Enough Coins";
+            //text.text = "Not Enough Coins";
+            min_coins.enabled = true;
            //# stars[0].SetActive(true);
         }
         else if (avgCoinsCollected >= 33 && avgCoinsCollected < 66)
         {
             stars[0].SetActive(true);
             nextButton.SetActive(true);
+            //min_coins.enabled = false;
         }
 
         else if (avgCoinsCollected >= 66 && avgCoinsCollected < 70)
@@ -43,6 +47,7 @@ public class starsHandler : MonoBehaviour
             stars[0].SetActive(true);
             stars[1].SetActive(true);
             nextButton.SetActive(true);
+            //min_coins.enabled = false;
 
         }
 
@@ -51,6 +56,7 @@ public class starsHandler : MonoBehaviour
             stars[0].SetActive(true);
             stars[1].SetActive(true);
             stars[2].SetActive(true);
+            //min_coins.enabled = false;
             nextButton.SetActive(true);
         }
 
