d011a849e2a19a9a53db10314ac6905fe52f220d
Soumyaprakash Dasmohaptra <dasmohap@usc.edu>
2022-11-07 21:31:25 -0800

    Made changes to scoreboard for level 4

--- Prototype/Assets/Scripts/LevelComplete.cs
+++ Prototype/Assets/Scripts/LevelComplete.cs
@@ -26,6 +26,7 @@ public class LevelComplete : MonoBehaviour
     {
         sendtogform.startSessionID = DateTime.Now;
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        Time.timeScale = 1f;
     }
 
     public void QuitToMainMenu()
