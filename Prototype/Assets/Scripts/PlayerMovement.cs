2ac50fcedc0e60d1f098b58aa5cb4dc669f5d191
Prateek Kumar <pkumar21@usc.edu>
2022-11-10 22:17:58 -0800

    walk animationj

--- Prototype/Assets/Scripts/PlayerMovement.cs
+++ Prototype/Assets/Scripts/PlayerMovement.cs
@@ -31,10 +31,15 @@ public class PlayerMovement : MonoBehaviour
     public float groundLength = 1.2f;
     public Vector3 colliderOffset;
 
+    public RuntimeAnimatorController restController;
+    public RuntimeAnimatorController walkController;
+
     void Start()
     {
         playerAnimation = GetComponent<Animator>();
-        playerAnimation.SetBool("OnGround", true);
+        playerAnimation.runtimeAnimatorController = restController;
+        // restController = Resources.Load("Assets/COPY SPRIGHT/2D Character - Astronaut/Animations/Player.anim") as RuntimeAnimatorController;
+        // walkController = Resources.Load("Assets/COPY SPRIGHT/2D Character - Astronaut/Animations/walk_side.anim") as RuntimeAnimatorController;
     }
 
     // Update is called once per frame
@@ -47,11 +52,6 @@ public class PlayerMovement : MonoBehaviour
         if(Input.GetButtonDown("Jump"))
         {
             jumpTimer = Time.time + jumpDelay;
-            playerAnimation.SetBool("OnGround", false);
-        }
-        else
-        {
-            playerAnimation.SetBool("OnGround", true);
         }
 
         direction = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
@@ -81,6 +81,14 @@ public class PlayerMovement : MonoBehaviour
         if(Mathf.Abs(rb.velocity.x) > maxSpeed)
         {
             rb.velocity = new Vector2(Mathf.Sign(rb.velocity.x) * maxSpeed, rb.velocity.y);
+        }
+
+        if(direction.x == 0)
+        {
+            playerAnimation.runtimeAnimatorController = restController;
+        } else
+        {
+            playerAnimation.runtimeAnimatorController = walkController;
         }
         //animator.SetFloat("horizontal", Mathf.Abs(rb.velocity.x));
     }
