3002d2e6a778087a69b8a17cdccf6f5258c53075
shrutika-singh <ssingh30@usc.edu>
2022-11-14 12:15:36 -0800

    changed variable name

--- Prototype/Assets/Scripts/DisappearingGround.cs
+++ Prototype/Assets/Scripts/DisappearingGround.cs
@@ -6,11 +6,11 @@ public class DisappearingGround : MonoBehaviour
 {
     public float toggleTime = 2;
     public float currentTime = 0;
-    public bool enabled = true;
+    public bool displayGround = true;
     // Start is called before the first frame update
     void Start()
     {
-        enabled = true;
+        displayGround = true;
     }
 
     // Update is called once per frame
@@ -25,11 +25,11 @@ public class DisappearingGround : MonoBehaviour
     }
     void TogglePlatform()
     {
-        enabled = !enabled;
+        displayGround = !displayGround;
         //gameObject.SetActive(enabled);
         foreach(Transform child in gameObject.transform)
         {
-            child.gameObject.SetActive(enabled);
+            child.gameObject.SetActive(displayGround);
         }
     }
 
