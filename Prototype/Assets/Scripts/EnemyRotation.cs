f9f9437f069bd061171befa9cf5601db93d177b1
Aditya Kapadia <adityakapadia@Adityas-MacBook-Pro.local>
2022-11-22 01:39:15 -0800

    Add moon pullforce

--- Prototype/Assets/Scripts/EnemyRotation.cs
+++ Prototype/Assets/Scripts/EnemyRotation.cs
@@ -18,16 +18,15 @@ public class EnemyRotation : MonoBehaviour
     {
         posX = RotationCenter.position.x + Mathf.Cos(angle) * rotationRadius;
         posY = RotationCenter.position.y + Mathf.Sin(angle) * rotationRadius;
-
+        // GameObject moon = GameObject.Find("Enemy");
+        // Debug.Log(angle/57.2);
+        // if (angle >=0 && angle <= 100)
+        // {
+        //     moon.SetActive(false);
+        // }
+        // moon.SetActive(true);
         transform.position = new Vector2(posX, posY);
         angle = angle + Time.deltaTime * angularSpeed;
-   
-       
-        GameObject moon = GameObject.Find("Enemy");
-        if (angle >=0 && angle <= 10)
-        {
-            //moon.SetActive(false);
-        }
 
     }
 
---
+++ Prototype/Assets/Scripts/Gravitation.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gravitation : MonoBehaviour
+{
+    public Transform player;
+    Rigidbody2D playerbody;
+    public float influencerange;
+    public float intensity;
+    public float dist2player;
+    Vector2 pullforce;
+    // [SerializeField] private float gravitationalForce = 5;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerbody = player.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        dist2player = Vector2.Distance(player.position, transform.position);
+        if(dist2player<=influencerange)
+        {
+            pullforce = (transform.position - player.position).normalized/(dist2player*intensity);
+            pullforce = pullforce*10;
+            Debug.Log(pullforce);
+            playerbody.AddForce(pullforce, ForceMode2D.Force);
+        }
+        // var directionOfBirdFromPlanet = (transform.position - player.position).normalized;
+
+            // Adds the force towards the center
+        // playerbody.AddForce(directionOfBirdFromPlanet * gravitationalForce);
+    }
+}
