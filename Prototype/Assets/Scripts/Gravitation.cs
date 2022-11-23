0a3baffb2dd36ac004b1282e662577eef7b16217
shrutika-singh <ssingh30@usc.edu>
2022-11-22 17:14:17 -0800

    gravitation effect removed before player movement

--- Prototype/Assets/Scripts/Gravitation.cs
+++ Prototype/Assets/Scripts/Gravitation.cs
@@ -10,6 +10,7 @@ public class Gravitation : MonoBehaviour
     public float intensity;
     public float dist2player;
     Vector2 pullforce;
+    public Vector2 direction;
     // [SerializeField] private float gravitationalForce = 5;
     // Start is called before the first frame update
     void Start()
@@ -20,14 +21,27 @@ public class Gravitation : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        dist2player = Vector2.Distance(player.position, transform.position);
-        if(dist2player<=influencerange)
+        direction = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        if (direction.x > 0)
         {
-            pullforce = (transform.position - player.position).normalized/(dist2player*intensity);
-            pullforce = pullforce*50;
-            Debug.Log(pullforce);
-            playerbody.AddForce(pullforce, ForceMode2D.Force);
+            
+            dist2player = Vector2.Distance(player.position, transform.position);
+
+            if (dist2player <= influencerange)
+            {
+                Debug.Log("Screen Width : " + UnityEngine.Screen.width);
+                Debug.Log("Player Position : " + direction.x);
+                Debug.Log("one/third : " + (UnityEngine.Screen.width) / 3);
+
+                pullforce = (transform.position - player.position).normalized / (dist2player * intensity);
+                pullforce = pullforce * 50;
+                Debug.Log(pullforce);
+                playerbody.AddForce(pullforce, ForceMode2D.Force);
+            }
+
         }
+
+        
         // var directionOfBirdFromPlanet = (transform.position - player.position).normalized;
 
             // Adds the force towards the center
